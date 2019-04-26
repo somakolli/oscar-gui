aa9d871f46c55028bf01538dd140d25daaf5489b
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2019-04-26 18:27:11 +0200

    add min item model

---
+++ src/app/models/oscar/oscar-min-item.spec.ts
@@ -0,0 +1,7 @@
+import { OscarMinItem } from './oscar-min-item';
+
+describe('OscarMinItem', () => {
+  it('should create an instance', () => {
+    expect(new OscarMinItem()).toBeTruthy();
+  });
+});
