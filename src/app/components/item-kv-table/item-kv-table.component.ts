bdaf7602e12ae71dcde6479e0384a5f2eca9c88a
somakolli <st116214@stud.uni-stuttgart.de>
2019-07-24 10:27:43 +0200

    add google and osm links

--- src/app/components/item-kv-table/item-kv-table.component.ts
+++ src/app/components/item-kv-table/item-kv-table.component.ts
@@ -10,8 +10,12 @@ export class ItemKvTableComponent implements OnInit {
   @Input() oscarItem: OscarItem;
   keyValues: object[] = [];
   constructor() { }
-
+  lat: number;
+  lon: number;
   ngOnInit() {
+    this.lat = this.oscarItem.bbox[2];
+    this.lon = this.oscarItem.bbox[0];
+    this.keyValues.push({k: 'osm-id', v: this.oscarItem.osmid});
     for (let i = 0; i < this.oscarItem.k.length; i++) {
       this.keyValues.push({k : this.oscarItem.k[i], v : this.oscarItem.v[i]});
       if (this.oscarItem.k[i] === 'name') {
